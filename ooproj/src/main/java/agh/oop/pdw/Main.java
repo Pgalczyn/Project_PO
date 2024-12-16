f5edc8ee4ce78446345009e131bd1a9fb7d87c7b
Szymon Migas <ataszymon@gmail.com>
2024-12-16 22:32:41 +0100

    changed main package name

---
+++ ooproj/src/main/java/agh/oop/pdw/Main.java
@@ -0,0 +1,13 @@
+package agh.oop.pdw;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.print("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+    }
+}
