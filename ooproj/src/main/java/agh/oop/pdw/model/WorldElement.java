47c17df588fc1f803fff8c1cef22049bcd3215bf
Szymon Migas <ataszymon@gmail.com>
2024-12-19 01:21:20 +0100

    Created world element interface

---
+++ ooproj/src/main/java/agh/oop/pdw/model/WorldElement.java
@@ -0,0 +1,11 @@
+package agh.oop.pdw.model;
+
+// Interfejs elementu świata
+public interface WorldElement {
+    // Zwracanie pozycji elementu
+    Vector2D getPosition();
+
+    // Wyświetlanie elementu
+    String toString();
+
+}
