857c43288a5fa4c0fb1d032a0829b3742f3b128b
Szymon Migas <ataszymon@gmail.com>
2024-12-19 01:20:30 +0100

    Basic logic for implementation with WorldMap

--- ooproj/src/main/java/agh/oop/pdw/model/Animal.java
+++ ooproj/src/main/java/agh/oop/pdw/model/Animal.java
@@ -1,6 +1,23 @@
 package agh.oop.pdw.model;
 
-public class Animal {
+import static agh.oop.pdw.model.MapDirection.randomDirection;
 
+public class Animal implements WorldElement{
+    private MapDirection direction;
+    private Vector2D position;
 
+    public Animal(Vector2D position) {
+        this.position = position;
+        this.direction = randomDirection();
+    }
+
+    @Override
+    public Vector2D getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString(){
+        return "Position: " + position.toString() + ", direction: " + direction.toString();
+    }
 }
