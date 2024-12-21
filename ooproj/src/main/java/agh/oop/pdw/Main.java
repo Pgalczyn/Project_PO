0278ac3c910c228303073a365b5392fd650c0c4a
Szymon Migas <ataszymon@gmail.com>
2024-12-21 17:23:41 +0100

    basic logic for simulation

--- ooproj/src/main/java/agh/oop/pdw/Main.java
+++ ooproj/src/main/java/agh/oop/pdw/Main.java
@@ -2,8 +2,10 @@ package agh.oop.pdw;
 
 
 import agh.oop.pdw.model.Animal;
+import agh.oop.pdw.model.Grass;
 import agh.oop.pdw.model.Vector2D;
 import agh.oop.pdw.model.WorldMap;
+import agh.oop.pdw.model.util.RandomUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,11 +13,20 @@ public class Main {
         Animal animal1 = new Animal(new Vector2D(0, 0));
         Animal animal2 = new Animal(new Vector2D(100, 100));
 
-        map.place(animal1);
-        map.place(animal2);
+        map.placeAnimal(animal1);
+        map.placeAnimal(animal2);
+
+        map.spawnGrass(new Grass(new Vector2D(3, 3)));
+
 
         for (Animal animal : map.getAnimals().values()) {
             System.out.println(animal);
         }
+
+        RandomUtils randomUtils = new RandomUtils();
+        for(int i = 0; i < 30; i++){
+             System.out.println(randomUtils.getGrassSpawnPosition(map));
+        }
+
     }
 }
---
+++ ooproj/src/main/java/agh/oop/pdw/model/util/RandomUtils.java
@@ -0,0 +1,32 @@
+package agh.oop.pdw.model.util;
+
+import agh.oop.pdw.model.Grass;
+import agh.oop.pdw.model.Vector2D;
+import agh.oop.pdw.model.WorldMap;
+
+import java.util.Map;
+import java.util.Random;
+
+public class RandomUtils {
+    private static final Random RANDOM = new Random();
+
+    public Vector2D getGrassSpawnPosition(WorldMap worldMap){
+        Map<Vector2D, Grass> elements = worldMap.getGrasses();
+        if (RANDOM.nextInt() < 80){
+            Boundary boundary = new Boundary(new Vector2D(30,30), new Vector2D(69,69));
+            Vector2D position = getRandomPosition(boundary);
+            while (elements.containsKey(position)){
+                position = getRandomPosition(boundary);
+            }
+            return position;
+        }
+        else return new Vector2D(-1, -1);
+    }
+
+    private Vector2D getRandomPosition(Boundary boundary){
+        return new Vector2D(
+                RANDOM.nextInt(boundary.topRight().getX() - boundary.bottomLeft().getX()) + boundary.bottomLeft().getX(),
+                RANDOM.nextInt(boundary.topRight().getY() - boundary.bottomLeft().getY()) + boundary.bottomLeft().getY()
+        );
+    }
+}
