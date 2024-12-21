bc3223f6bb902787172e0e9fadb3658a768f90ff
pawciogaa <pawciogaa@gmail.com>
2024-12-21 01:21:19 +0100

    intial work on Animal, not ready yet

---
+++ ooproj/src/main/java/agh/oop/pdw/Simulation.java
@@ -0,0 +1,15 @@
+package agh.oop.pdw;
+
+public class Simulation {
+
+    private final int lengthOfGenotype;
+    private final int amountOfAnimals;
+    private final int startingAmountOfEnergyAnimals;
+
+    public Simulation(int lengthOfGenotype, int amountOfAnimals, int startingAmountOfEnergyAnimals) {
+        this.lengthOfGenotype = lengthOfGenotype;
+        this.amountOfAnimals = amountOfAnimals;
+        this.startingAmountOfEnergyAnimals = startingAmountOfEnergyAnimals;
+
+    }
+}
--- ooproj/src/main/java/agh/oop/pdw/model/Animal.java
+++ ooproj/src/main/java/agh/oop/pdw/model/Animal.java
@@ -1,14 +1,49 @@
 package agh.oop.pdw.model;
 
+import java.util.Random;
+
 import static agh.oop.pdw.model.MapDirection.randomDirection;
 
 public class Animal implements WorldElement{
     private MapDirection direction;
     private Vector2D position;
+    private int currentEnergy;
+    private int[] genotype;
+    private int lengthOfGenotype;
+    private int activeGene;
+    private int amountOfEatenPlants = 0;
+    private int amountOfChildren = 0;
+    private int amountOfDescendants = 0;
+    private int amountOfDaysAlive = 0;
+    private int amountOfDaysUntilDeath = Integer.MAX_VALUE;
 
-    public Animal(Vector2D position) {
+
+    //constructor for aniamals that were born on the map
+    public Animal(Vector2D position, int StartEnergy, int[] genotype) {
+        this.position = position;
+        this.genotype = genotype;
+        this.lengthOfGenotype = genotype.length;
+        this.direction = randomDirection();
+        this.currentEnergy = StartEnergy;
+
+        Random rand = new Random();
+        this.activeGene = rand.nextInt(8);
+    }
+    //constructor for aniamls which we put on the map
+    public Animal(Vector2D position, int StartEnergy,int LengthOfGenotype) {
         this.position = position;
         this.direction = randomDirection();
+        this.currentEnergy = StartEnergy;
+        this.lengthOfGenotype = LengthOfGenotype;
+
+        Random rand = new Random();
+        int[] genotyp = new int [lengthOfGenotype];
+        for (int i =0 ; i < lengthOfGenotype; i++) {
+            genotyp[i] = rand.nextInt(8);
+        }
+        this.genotype = genotype;
+        this.activeGene = rand.nextInt(8);
+
     }
 
     @Override
