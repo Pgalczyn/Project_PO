06e5211e855ce575c695d8c6d89167a293bdd47d
Szymon Migas <ataszymon@gmail.com>
2025-01-07 19:44:06 +0100

    created tests for world map

---
+++ ooproj/src/test/java/agh/oop/pdw/model/WorldMapTest.java
@@ -0,0 +1,79 @@
+package agh.oop.pdw.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class WorldMapTest {
+    @Test
+    public void animalCanMoveValidation(){
+        WorldMap map = new WorldMap(10,10, 10);
+        Animal animal = new Animal(new Vector2D(0,0),1, 1, 0);
+        animal.setGenotype(new int[]{0});
+        animal.setDirection(MapDirection.SOUTH);
+        map.placeAnimal(animal);
+        assertFalse(map.canMoveTo(getAnimalNextPosition(animal)));
+
+        animal.setDirection(MapDirection.SOUTH_WEST);
+        assertFalse(map.canMoveTo(getAnimalNextPosition(animal)));
+
+        animal.setDirection(MapDirection.WEST);
+        assertTrue(map.canMoveTo(getAnimalNextPosition(animal)));
+
+        Animal animal2 = new Animal(new Vector2D(0,9),1, 1, 0);
+        animal2.setDirection(MapDirection.WEST);
+        assertTrue(map.canMoveTo(getAnimalNextPosition(animal2)));
+
+        animal2.setDirection(MapDirection.NORTH_EAST);
+        assertFalse(map.canMoveTo(getAnimalNextPosition(animal2)));
+    }
+
+    @Test
+    public void testAnimalPlace() {
+        WorldMap map = new WorldMap(10,10, 10);
+        Animal animal = new Animal(new Vector2D(0,0),1, 1, 0);
+        map.placeAnimal(animal);
+        assertTrue(map.getAnimals().containsKey(animal.getPosition()));
+
+        Animal animal2 = new Animal(new Vector2D(0,0),1, 1, 0);
+        map.placeAnimal(animal2);
+        assertEquals(2, map.getAnimals().get(animal.getPosition()).length);
+    }
+
+    @Test
+    public void testGrassSpawn(){
+        WorldMap map = new WorldMap(10,10, 0);
+        map.spawnGrass();
+        assertEquals(1, map.getGrasses().size());
+
+        WorldMap map2 = new WorldMap(1,1,0);
+        map2.spawnGrass();
+        assertEquals(1, map2.getGrasses().size());
+        map2.spawnGrass();
+        map2.spawnGrass();
+        assertEquals(1, map2.getGrasses().size());
+        assertEquals(0, map2.getEmptyFields().size());
+    }
+
+    @Test
+    public void testAnimalNextPosition() {
+        WorldMap map = new WorldMap(3, 3, 0);
+        Animal animal = new Animal(new Vector2D(0,0),1, 1, 0);
+        animal.setDirection(MapDirection.WEST);
+        assertEquals(new Vector2D(2,0), map.getNextPosition(animal));
+
+        Animal animal2 = new Animal(new Vector2D(2,2),1, 1, 0);
+        animal2.setDirection(MapDirection.NORTH_EAST);
+        assertEquals(new Vector2D(2, 2), map.getNextPosition(animal2));
+
+        Animal animal3 = new Animal(new Vector2D(2,0),1, 1, 0);
+        animal3.setDirection(MapDirection.EAST);
+        assertEquals(new Vector2D(0,0), map.getNextPosition(animal3));
+    }
+
+
+
+    private Vector2D getAnimalNextPosition(Animal animal){
+        return animal.getPosition().addVector(animal.getDirection().toVector());
+    }
+}
